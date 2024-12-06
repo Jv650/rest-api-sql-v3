e0f585db5ccf5c92b53141760504d05ced01d1e8
Jv650 <141464576+Jv650@users.noreply.github.com>
2024-12-06 15:31:47 -0800

    edits

--- routes/course.js
+++ routes/course.js
@@ -6,28 +6,30 @@ const { User } = require("../models");
 const router = express.Router();
 const { authenticateUser } = require("../middleware/user-auth");
 
-function asyncHandler(cb) {
-  return async (req, res, next) => {
-    try {
-      await cb(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  };
-}
+// function asyncHandler(cb) {
+//   return async (req, res, next) => {
+//     try {
+//       await cb(req, res, next);
+//     } catch (err) {
+//       next(err);
+//     }
+//   };
+// }
 
-///api/courses - GET:
-//Return all courses including the User object associated with each course and
-//a 200 HTTP status code.
-router.get("/api/courses", (req, res) => {
-  res.json(course);
-  res.status(200).json({ message: "Courses successfully retrieved!" });
-  res.json({
-    title: res.body.title,
-    description: res.body.description,
-    estimatedTime: res.body.estimatedTime,
-    materialsNeeded: res.body.materialsNeeded,
+// ///api/courses - GET:
+// //Return all courses including the User object associated with each course and
+// //a 200 HTTP status code.
+router.get("/", async (req, res) => {
+  const course = await Course.findAll({
+    include: [{ model: User }],
   });
+  res.status(200).json({ message: "Courses successfully retrieved!" });
+  //   res.json({
+  //     title: res.body.title,
+  //     description: res.body.description,
+  //     estimatedTime: res.body.estimatedTime,
+  //     materialsNeeded: res.body.materialsNeeded,
+  //   });
 
   if (course) {
     res.json(course);
@@ -37,84 +39,124 @@ router.get("/api/courses", (req, res) => {
   }
 });
 
-///api/courses/:id - GET:
-//Return the corresponding course including the User object associated with that course and
-//a 200 HTTP status code
-router.get("/api/courses/:id", (req, res) => {
-  const courses = req.currentCourse; //req.body.course??
-  const user = req.currentUser; //req.body.user??
-  if (courses) {
-    res.json(course);
-    res.json(user);
-    res.json({ courses: courses, users: users });
-    res.status(200).json({ message: "Course successfully retrieved!" });
-  } else {
-    res.status(400);
-    res.json({ message: "No course found" });
+// ///api/courses/:id - GET:
+// //Return the corresponding course including the User object associated with that course and
+// //a 200 HTTP status code
+router.get("/:id", async (req, res) => {
+  try {
+    //the await will wait and find the quote based on what the user inputs as the :id
+    const course = await Course.findByPk(req.params.userId);
+    if (course) {
+      res.json(course);
+    } else {
+      res.status(404).json({ message: "Course not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
-///api/courses - POST:
-//Create a new course, set the Location header to the URI for the newly created course,
-//and return a 201 HTTP status code and no content
-router.post("/api/courses", async (req, res) => {
+// ///api/courses - POST:
+// //Create a new course, set the Location header to the URI for the newly created course,
+// //and return a 201 HTTP status code and no content
+// router.post("/", async (req, res) => {
+//   try {
+//     const course = await Course.create(req.body);
+//     res.status(201).location(`/api/courses/${course.id}`).end();
+//   } catch (err) {
+//     console.log("Error: ", error.name);
+//     if (
+//       error.name === "SequelizeValidationError" ||
+//       error.name === "SequelizeUniqueConstraintError"
+//     ) {
+//       const errors = error.errors.map((err) => err.message);
+//       res.status(400).json({ errors });
+//     } else {
+//       throw error;
+//     }
+//   }
+// });
+
+router.post(
+  "/",
+  //asyncHandler(
+  async (req, res) => {
+    try {
+      await Course.create(req.body);
+      res.status(201).json({ message: "Course successfully created!" });
+    } catch (error) {
+      if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+      ) {
+        const errors = error.errors.map((err) => err.message);
+        res.status(400).json({ errors });
+      } else {
+        throw error;
+      }
+    }
+  }
+);
+
+// // ///api/courses/:id - PUT:
+// // //Update the corresponding course and
+// // //return a 204 HTTP status code and no content
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const getCourse = await Course.findByPk(req.params.userId);
+//     if (getCourse) {
+//       //set course objects property equal to the new course property sent to us by the client
+//       getCourse.course = req.body.course;
+//       await getCourse.update({
+//         title: req.body.title,
+//         description: req.body.description,
+//         estimatedTime: req.body.estimatedTime,
+//         materialsNeeded: req.body.materialsNeeded,
+//         userId: req.body.userId,
+//       });
+
+//       res.status(204).end(); //for put req, it's best practice to send a 204 status code which means everything was sent ok but there's nothing to send back
+//     } else {
+//       res.status(404).json({ message: "Course not found" });
+//     }
+//   } catch (error) {
+//     //res.status(500).json({ message: err.message }); //you can inidicate your own status codes
+//     if (
+//       error.name === "SequelizeValidationError" ||
+//       error.name === "SequelizeUniqueConstraintError"
+//     ) {
+//       const errors = error.errors.map((error) => error.message);
+//       res.status(400).json({ errors });
+//     } else {
+//       throw error;
+//     }
+//   }
+// });
+
+router.put("/:id", async (req, res) => {
   try {
-    //will check if user and course have been submitted with if statement
-    if (req.body.users && req.body.courses) {
-      //throw new Error("Oh no something went wrong!");
-      const course = await course.createCourse({
-        //here is the object which must be passed a course and user property by the user
-        course: req.body.courses,
-        user: req.body.users,
-      });
-      res.status(201).json(course);
-      res.redirect("Location", "/"); //edit??
+    const getCourse = await Course.create(req.body); //await records.getQuote(req.params.id);
+    if (getCourse) {
+      //set quote objects quote property equal to the new quote property sent to us by the client
+      getCourse.title = req.body.title;
+      getCourse.description = req.body.description;
+      getCourse.estimatedTime = req.body.estimatedTime;
+      getCourse.materialsNeeded = req.body.materialsNeeded;
+
+      await Course.update(getCourse); //updateQuote method will save the new quote to the datastore - since its an async function you need to await it as well
+      res.status(204).end(); //for put req, it's best practice to send a 204 status code which means everything was sent ok but there's nothing to send back
     } else {
-      res.status(400).json({ message: "Course and user required." });
+      res.status(404).json({ message: "Course not found" });
     }
   } catch (err) {
     res.status(500).json({ message: err.message }); //you can inidicate your own status codes
   }
 });
 
-///api/courses/:id - PUT:
-//Update the corresponding course and
-//return a 204 HTTP status code and no content
-router.put("/api/courses/:id", async (req, res) => {
-  try {
-    const getCourse = await course.getCourse(req.params.userId);
-    if (getCourse) {
-      //set course objects property equal to the new course property sent to us by the client
-      getCourse.course = req.body.course;
-      await courses.update({
-        title: req.body.title,
-        description: req.body.description,
-        estimatedTime: req.body.estimatedTime,
-        materialsNeeded: req.body.materialsNeeded,
-      });
-
-      res.status(204).end(); //for put req, it's best practice to send a 204 status code which means everything was sent ok but there's nothing to send back
-    } else {
-      res.status(404).json({ message: "Course not found" });
-    }
-  } catch (error) {
-    //res.status(500).json({ message: err.message }); //you can inidicate your own status codes
-    if (
-      error.name === "SequelizeValidationError" ||
-      error.name === "SequelizeUniqueConstraintError"
-    ) {
-      const errors = error.errors.map((error) => error.message);
-      res.status(400).json({ errors });
-    } else {
-      throw error;
-    }
-  }
-});
-
-///api/courses/:id - DELETE:
-//Delete the corresponding course and
-//return a 204 HTTP status code and no content
-router.delete("/api/courses/:id", async (req, res) => {
+// ///api/courses/:id - DELETE:
+// //Delete the corresponding course and
+// //return a 204 HTTP status code and no content
+router.delete("/:id", async (req, res) => {
   try {
     const course = await course.getCourse(req.params.userId);
     await course.deleteCourse(course);
--- routes/user.js
+++ routes/user.js
@@ -3,55 +3,66 @@ const express = require("express");
 const { User } = require("../models");
 // Construct a router instance??
 const router = express.Router();
-var bcrypt = require("bcryptjs");
 const { authenticateUser } = require("../middleware/user-auth");
 
 //Handler function to wrap routes and to allow proper usager of asyncHandler
-function asyncHandler(cb) {
-  return async (req, res, next) => {
-    try {
-      await cb(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  };
-}
+// function asyncHandler(cb) {
+//   return async (req, res, next) => {
+//     try {
+//       await cb(req, res, next);
+//     } catch (err) {
+//       next(err);
+//     }
+//   };
+// }
+
 ///api/users - GET:
 //This will return all properties and values for the currently authenticated User
 //along with a 200 HTTP status code
-router.get(
-  "/api/users",
-  authenticateUser,
-  asyncHandler(async (req, res) => {
-    const user = req.currentUser;
-    res.json(user); //return all properties from  user.js models
-    res.status(200).json({ message: "User successfully retrieved!" });
-    res.json({
-      firstName: user.firstName,
-      lastName: user.lastName,
-      emailAddress: user.emailAddress,
-      password: user.password,
-    });
-  })
-);
+// router.get(
+//   "/",
+//authenticateUser,
+//asyncHandler(
+//   async (req, res) => {
+//     const user = req.currentUser;
+//     res.json(user); //return all properties from  user.js models
+//     res.status(200).json({ message: "User successfully retrieved!" });
+//     res.json({
+//       firstName: user.firstName,
+//       lastName: user.lastName,
+//       emailAddress: user.emailAddress,
+//       password: user.password,
+//     });
+//   }
+// );
+//);
+router.get("/", async (req, res) => {
+  try {
+    //async and await to make sure we have our user data before we respond to the client.
+    const user = res.currentUser;
+    res.status(200).json(user);
+    // json({
+    //   firstName: user.firstName,
+    //   lastName: user.lastName,
+    //   emailAddress: user.emailAddress,
+    //   password: user.password,
+    // });
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+});
 
 ///api/users - POST:
 //This route should create a new user, set the Location header to "/",
 //and return a 201 HTTP status code and no content.
 router.post(
-  "/api/users",
-  asyncHandler(async (req, res) => {
-    console.log(req.body);
+  "/",
+  //asyncHandler(
+  async (req, res) => {
+    //console.log(req.body);
     try {
       await User.create(req.body);
-      //   await User.create({
-      //     firstName: req.user.firstName,
-      //     lastName: req.user.lastName,
-      //     emailAddress: req.user.emailAddress,
-      //     password: req.user.password,
-      //   });
-      res.status(201).json({ message: "Account successfully created!" });
-      res.redirect("Location", "/");
+      res.status(201).location("/").end();
     } catch (error) {
       if (
         error.name === "SequelizeValidationError" ||
@@ -63,7 +74,8 @@ router.post(
         throw error;
       }
     }
-  })
+  }
 );
+//);
 
 module.exports = router;
